9d11d042a1b5c6720869bcb2705125039c099619
Adama DIENG <adama.dieng@digitastuces.com>
2023-03-20 14:58:43 +0100

    Flask

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+app_instance_name = 'myapp'
+app_server_name = 'app1'
+
+@app.route("/")
+def hello():
+    return '<h1>Hello from {} on server {}!</h1>'.format(app_instance_name, app_server_name)
+
+@app.route("/health")
+def health():
+    return jsonify({'status': 'ok'})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
